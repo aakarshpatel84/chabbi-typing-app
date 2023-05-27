0f90a1eb21828123110b6bcf30d53df865531ab4
My Name <aakarshpatel84@gmail.com>
2023-05-27 11:21:33 +0530

    Cagrigated code in components

--- src/components/Typing-App/TypingApp.jsx
+++ src/components/Typing-App/TypingApp.jsx
@@ -1,22 +1,55 @@
 import React from "react";
 import { useState, useRef } from "react";
 import "./TypingApp.css";
+import Word from "../Word/Word";
 const getCloud = () =>
-  `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`
-    .split(" ")
-    .sort(() => (Math.random() > 0.5 ? 1 : -1));
+  `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`.split(
+    " "
+  );
+
 function TypingApp() {
   const [userInput, setuserInput] = useState("");
+  const [activeWordIndex, setActiveWordIndex] = useState(0);
+  const [correctWordArr, setcorrectWordArr] = useState([]);
   const cloud = useRef(getCloud());
+
+  const processInput = (value) => {
+    if (value.endsWith(" ")) {
+      setActiveWordIndex((index) => index + 1);
+      setuserInput("");
+
+      // correct word
+      setcorrectWordArr((data) => {
+        const word = value.trim();
+        const newResult = [...data];
+        newResult[activeWordIndex] = word === cloud.current[activeWordIndex];
+        return newResult;
+      });
+    } else {
+      setuserInput(value);
+    }
+  };
+
   return (
     <div className="App">
       <div className="text-pera">
-        <p>{cloud.current.join(" ")}</p>
+        <p>
+          {cloud.current.map((words, index) => {
+            return (
+              <Word
+                text={words}
+                key={index}
+                active={index === activeWordIndex}
+                correct={correctWordArr[index]}
+              />
+            );
+          })}
+        </p>
       </div>
       <input
         type="text"
         value={userInput}
-        onChange={(e) => setuserInput(e.target.value)}
+        onChange={(e) => processInput(e.target.value)}
       />
     </div>
   );
